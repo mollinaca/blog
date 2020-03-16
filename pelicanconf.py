a45c9b1b55ad3672eeec81f844594f6c23031713
hstn <github@mail.watarinohibi.tokyo>
2020-03-17 00:12:04 +0900

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 
 AUTHOR = 's.hosoya'
 SITENAME = '渡りの日々.'
-SITEURL = ''
+SITEURL = 'https://blog.watarinohibi.tokyo'
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Tokyo'
 
-DEFAULT_LANG = 'jp'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +20,18 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/syoutin'),
+          ('github','https://github.com/mollinaca'))
 
-DEFAULT_PAGINATION = 3
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True

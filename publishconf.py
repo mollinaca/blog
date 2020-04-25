bf1470e9b534813dcdb61450104e1c3b6fbc37b7
mollinaca <github@mail.watarinohibi.tokyo>
2020-04-25 21:23:15 +0900

    modify theme変更、調整など

--- publishconf.py
+++ publishconf.py
@@ -2,13 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# General Settings
 AUTHOR = 's.hosoya'
-SITENAME = '渡りの日々.'
-SITEURL = 'https://blog.watarinohibi.tokyo'
+SITENAME = "Days of migratory birds" 
+SITEURL = 'http://watarinohibi.tokyo' # for Production
 PATH = 'content'
 TIMEZONE = 'Asia/Tokyo'
-DEFAULT_LANG = 'en'
-RELATIVE_URLS = False
+DEFAULT_LANG = 'ja'
+RELATIVE_URLS = True
+DEFAULT_PAGINATION = 3
+
+# TIMEZONE
+TIMEZONE = 'Asia/Tokyo'
+DATE_FORMATS = {
+    'en': '%a, %d %b %Y',
+    'ja': '%Y-%m-%d(%a)',
+}
+
+# Site setting
+USE_FOLDER_AS_CATEGORY = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_PAGES_ON_MENU = True
+
+# Site PATH setting
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,7 +40,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
+#         ('Python.org', 'http://python.org/')
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
 #         ('You can modify those links in your config file', '#'),)
 
@@ -27,29 +48,14 @@ AUTHOR_FEED_RSS = None
 SOCIAL = (('twitter', 'https://twitter.com/syoutin'),
           ('github','https://github.com/mollinaca'))
 
-DEFAULT_PAGINATION = 1
+# Plugins
+PLUGIN_PATHS = ['../pelican-plugins/']
+PLUGINS = ['tag_cloud']
 
-# Additional
-#::python
-TIMEZONE = 'Asia/Tokyo'
-DATE_FORMATS = {
-    'en': '%a, %d %b %Y',
-    'ja': '%Y-%m-%d(%a)',
-}
-
-ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
-USE_FOLDER_AS_CATEGORY = True
+## add recent page
+## http://www.voidynullness.net/blog/2015/06/25/add-recent-posts-list-to-pelican-blog/
+#TEMPLATE_PAGES = {'recent.json': 'api/recent.json', }
 
 # Theme
-THEME = "../pelican-themes/foundation-default-colours"
-
-# theme conf
-FOUNDATION_FRONT_PAGE_FULL_ARTICLES = True
-FOUNDATION_ALTERNATE_FONTS = True
-FOUNDATION_TAGS_IN_MOBILE_SIDEBAR = True
-#FOUNDATION_NEW_ANALYTICS = False
-#FOUNDATION_ANALYTICS_DOMAIN = ''
-FOUNDATION_FOOTER_TEXT = 'Powered by <a href="http://getpelican.com">Pelican</a> and <a href="http://foundation.zurb.com/">Zurb Foundation</a>. Theme by <a href="http://hamaluik.com">Kenton Hamaluik</a>.'
-FOUNDATION_PYGMENT_THEME = 'monokai'
-MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+# https://github.com/getpelican/pelican-themes/tree/master/new-bootstrap2
+THEME = "themes/new-bootstrap2"

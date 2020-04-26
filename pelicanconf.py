ad7e485b8e05cc0ca3f726a44f23ff4076f0f180
mollinaca <github@mail.watarinohibi.tokyo>
2020-04-26 17:54:39 +0900

    add SLUG_REGEX_SUBSTITUTIONS

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
+from unidecode import unidecode
 
 # General Settings
 AUTHOR = 's.hosoya'
 SITENAME = "Days of migratory birds" 
 SITEURL = 'http://localhost:8000' # for localhost test
 PATH = 'content'
-TIMEZONE = 'Asia/Tokyo'
-DEFAULT_LANG = 'ja'
+ROBOTS = 'index, follow'
+COPYRIGHT_YEAR = datetime.now().year
 RELATIVE_URLS = True
 DEFAULT_PAGINATION = 3
+LOAD_CONTENT_CACHE = False
+SITELOGO = 'https://blog.watarinohibi.tokyo/images/static/profile.png'
+#FAVICON = 'https://blog.watarinohibi.tokyo/images/static/favicon.ico'
 
-# TIMEZONE
+# TZ & Locale
 TIMEZONE = 'Asia/Tokyo'
 DATE_FORMATS = {
     'en': '%a, %d %b %Y',
     'ja': '%Y-%m-%d(%a)',
 }
+DEFAULT_LANG = 'ja'
 
 # Site setting
+MAIN_MENU = True
+MENUITEMS = (
+  ('Archives', '/archives.html'),
+  ('Categories', '/categories.html'),
+  ('Tags', '/tags.html'),
+)
 USE_FOLDER_AS_CATEGORY = True
-DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
 DISPLAY_TAGS_ON_SIDEBAR = True
 DISPLAY_PAGES_ON_MENU = True
+CC_LICENSE = {
+  'name': 'Creative Commons Attribution-ShareAlike',
+  'version': '4.0',
+  'slug': 'by-sa',
+}
 
 # Site PATH setting
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}'
@@ -49,13 +66,35 @@ SOCIAL = (('twitter', 'https://twitter.com/syoutin'),
           ('github','https://github.com/mollinaca'))
 
 # Plugins
-PLUGIN_PATHS = ['../pelican-plugins/']
-PLUGINS = ['tag_cloud']
+PLUGIN_PATHS = ['plugins/', 'plugins-custom/']
+PLUGINS = ['tag_cloud',
+           'sitemap',
+           'related_posts',
+           'better_codeblock_line_numbering',
+           'code_include']
+SITEMAP = {
+    'format': 'xml'
+}
+RELATED_POSTS_MAX = 5
+TAG_JAPANESE = True
 
-## add recent page
-## http://www.voidynullness.net/blog/2015/06/25/add-recent-posts-list-to-pelican-blog/
-#TEMPLATE_PAGES = {'recent.json': 'api/recent.json', }
+# Tag Substitutions
+SLUG_REGEX_SUBSTITUTIONS = (
+    (unidecode('タグテスト'), 'tag_test'),
+    (unidecode('雑記'), 'diary'),
+    (unidecode('ニュース'), 'news'),
+    (unidecode('時事'), 'current_news'),
+    (unidecode('政治'), 'politics'),
+    (unidecode('思考実験'), 'te'),
+    (unidecode('ゲーム'), 'game'),
+    (unidecode('プログラミング'), 'p'),
+    (unidecode('競技プログラミング'), 'cp')
+    )
 
 # Theme
-# https://github.com/getpelican/pelican-themes/tree/master/new-bootstrap2
-THEME = "themes/new-bootstrap2"
+# https://github.com/alexandrevicenzi/Flex
+THEME = "themes/Flex"
+PYGMENTS_STYLE = 'monokai'
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+USE_LESS = True
